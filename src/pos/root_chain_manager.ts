d2d6be7bcf0646a33598ac060410f75d5cacdcfa
Aman Zishan <amanzishan.az@gmail.com>
2022-06-21 16:51:22 +0530

    Updates

--- src/pos/root_chain_manager.ts
+++ src/pos/root_chain_manager.ts
@@ -1,9 +1,9 @@
-import { BaseToken, Web3SideChainClient } from '../utils'
-import { IPOSClientConfig, ITransactionOption } from '../interfaces'
+import { BaseToken, Web3SideChainClient } from '../utils';
+import { IPOSClientConfig, ITransactionOption } from '../interfaces';
 
 export class RootChainManager extends BaseToken<IPOSClientConfig> {
   constructor(client_: Web3SideChainClient<IPOSClientConfig>, address: string) {
-    console.log('Root Chain Manager address = ', address)
+    console.log('Root Chain Manager address = ', address);
     super(
       {
         address: '0xD7ecbfE71A9d643Fc8d8868E224474864e42A483',
@@ -12,30 +12,30 @@ export class RootChainManager extends BaseToken<IPOSClientConfig> {
         isParent: true,
       },
       client_
-    )
+    );
   }
 
   method(methodName: string, ...args) {
     return this.getContract().then(contract => {
-      return contract.method(methodName, ...args)
-    })
+      return contract.method(methodName, ...args);
+    });
   }
 
   deposit(userAddress: string, tokenAddress: string, depositData: string, option?: ITransactionOption) {
     return this.method('depositFor', userAddress, tokenAddress, depositData).then(method => {
-      return this.processWrite(method, option)
-    })
+      return this.processWrite(method, option);
+    });
   }
 
   exit(exitPayload: string, option: ITransactionOption) {
     return this.method('exit', exitPayload).then(method => {
-      return this.processWrite(method, option)
-    })
+      return this.processWrite(method, option);
+    });
   }
 
   isExitProcessed(exitHash: string) {
     return this.method('processedExits', exitHash).then(method => {
-      return this.processRead<boolean>(method)
-    })
+      return this.processRead<boolean>(method);
+    });
   }
 }
