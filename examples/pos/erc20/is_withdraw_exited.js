72e1ffa33e11a9f41b15da66f92ba6ea3fb0e009
Dias <dias@littlebit.sg>
2022-06-21 13:05:29 +0530

    Updated with audited address for chain manager

--- examples/pos/erc20/is_withdraw_exited.js
+++ examples/pos/erc20/is_withdraw_exited.js
@@ -4,7 +4,7 @@ const { getPOSClient, from } = require('../../utils');
 const execute = async () => {
     const client = await getPOSClient();
     const erc20Token = client.erc20(pos.parent.erc20, true);
-    const isExited = await erc20Token.isWithdrawExited('0xb005d8db45f33836c422ee18286fa8ebe49b4ec7b9930e673d85ecd081cc3b8e');
+    const isExited = await erc20Token.isWithdrawExited('0x548a88d6af6633537def2b3b5deb311198ece649f6aa4ade9ec574ff0fda7698');
 
     console.log("result", isExited);
 }
